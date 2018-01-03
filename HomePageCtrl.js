d0d41261786d2b99ca5238b57d0028dbe12f50b2
AkshathaKShetty <31784177+AkshathaKShetty@users.noreply.github.com>
2018-01-03 17:31:52 +0530

    Add files via upload

---
+++ HomePageCtrl.js
@@ -0,0 +1,44 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var myHome = angular.module("myHome",[]);
+myHome.controller("HomePageCtrl",function($scope,$state){
+    
+    $scope.LogOut = function(){
+        $state.go('Logout');
+    };
+    
+    $scope.items = [
+            { Name: "Gummies", Price: "55", Rating: "1", Image: "../html/Gummies.jpeg" },
+            { Name: "Cookies", Price: "70", Rating: "5", Image: "../html/Cookies.jpeg" },
+            { Name: "Sugar Free", Price: "40", Rating: "6", Image: "../html/Sugar Free.jpeg" },
+            { Name: "Assorted Biscuits", Price: "40", Rating: "9", Image: "../html/Assorted Biscuits.jpeg" }];
+        
+    $scope.movies =[
+        { Name :"IceAge", Image:"../html/IceAge.jpeg", Price:"300",Rating:"5", Hall:""},
+        { Name :"Kumfu Panda", Image:"../html/Kumfu Panda.jpeg",Price:"300", Rating:"3",Hall:""}];
+
+ $scope.search = function (item) {
+                if ($scope.searchText == undefined) {
+                    return true;
+                }
+                else {
+                    if (item.Name.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1 || 
+                            item.Price.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1||
+                            item.Rating.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1)
+                        {
+                            return true;
+                        }
+                }
+
+                return false;
+            };
+
+});
+
+
+
+
